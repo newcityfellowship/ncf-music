2b123b0be7b368a9806443c1a90a28ae96b1cce9
Robert Righter <robrighter@gmail.com>
2010-11-10 21:35:20 -0500

    Hooked up the audio player progress bar and time counter

--- frank/static/js/site.js
+++ frank/static/js/site.js
@@ -24,15 +24,30 @@ $(document).ready(function (){
   });
   
   //setup the audio players
-  $('.mp3holder').each(function(item){
-    console.log(this);
-    var mp3file = $(this).attr('rel');
-    $(this).jPlayer( {
+  $('.audioplayer').each(function(item){
+    var mp3holder = $(this).find('.mp3holder');
+    var progressbar = $(this).find('.innerprogressbar');
+    var timeinfo = $(this).find('.time');
+    var playbutton = $('#play'+mp3holder.attr('id'));
+    var mp3file = mp3holder.attr('rel');
+    mp3holder.jPlayer( {
       ready: function () {
         console.log('setting up player for song: ' + mp3file);
         mp3file && this.element.jPlayer("setFile", mp3file); // Defines the mp3
       }
     });
+    
+    mp3holder.jPlayer("onProgressChange", function(lp,ppr,ppa,pt,tt) {
+      progressbar.css('width', ppa+"%");
+      timeinfo.html(Math.round(pt/1000) + "/" + Math.round(tt/1000));
+    });
+    
+    mp3holder.jPlayer("onSoundComplete", function() {
+      timeinfo.html('0/0');
+      progressbar.css('width', "0%");
+      playbutton.removeClass('playing').addClass('paused');
+    });
+    
   });
   
   //setup the click handlers for the play buttons
